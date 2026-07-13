e02e8787e814b3307c4e79d1ca6bf881531a48bb
CapriBlu <pawel.petruk@hotmail.com>
2026-07-13 09:32:37 +0200

    Aggiunge struttura iniziale e acquisizione schermo

---
+++ src/main.py
@@ -0,0 +1,13 @@
+﻿from screen_capture import capture_screen
+
+
+def main():
+    print("Pixel Bot avviato")
+
+    path = capture_screen()
+
+    print(f"Screenshot salvato in: {path}")
+
+
+if __name__ == "__main__":
+    main()
